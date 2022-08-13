83f4c3a159b8da20dbbcf7431165ae1a9e303007
yammmt <cu.cs.y20@gmail.com>
2022-08-13 21:09:25 +0900

    AHC013: Remove inefficient orphaned vertex check
    score: 34058 -> 128748

--- ahc013/src/bin/a.rs
+++ ahc013/src/bin/a.rs
@@ -2,7 +2,7 @@ use proconio::input;
 use proconio::marker::Chars;
 use rand::rngs::SmallRng;
 use rand::{Rng, SeedableRng};
-use std::collections::{HashSet, VecDeque};
+use std::collections::VecDeque;
 use std::fmt;
 use std::time::{Duration, Instant};
 
@@ -173,86 +173,11 @@ fn main() {
     // とりあえず正の得点を取る
     // N <= 48 より O(N^4) なら間に合うし結ぶ際には距離は不問なので右/下全部見ても平均的には良化しそう
 
-    // 一度結べるだけ結んだ後に孤児を移動して近いグループに入れてみる
-    // 接続判定が微妙に変わるので解答作成もやり直す
-    // ちまちませず巨大なグループを作るべきではあるが賢い方法が浮かばないので保留
-
     let max_k = 100 * k;
     let mut x_move: Vec<Move> = vec![];
 
-    // 一度愚直結合
-    let (y_connect, cable) = greedy_ans(max_k, &cnn);
-
-    // 孤児をどこかの組に混ぜるために移動する (BFS)
-    // 移動後に再度愚直結合してやる
-    // HACK: 手数制限のために "移動したのにマージできない" 場合にはスコアが悪化する
-    let mut is_orphaned = vec![vec![true; n]; n];
-    for y in &y_connect {
-        is_orphaned[y.0][y.1] = false;
-        is_orphaned[y.2][y.3] = false;
-    }
-
-    for i in 0..n {
-        for j in 0..n {
-            if !is_orphaned[i][j] || cnn[i][j] == '0' {
-                continue;
-            }
-
-            let mut visited = HashSet::new();
-            // ((今の座標), これまでの経路)
-            // 経路クローンが重いが盤面サイズ N が小さいので間に合うはず
-            let mut que = VecDeque::new();
-            que.push_back(((i, j), vec![(i, j)]));
-            visited.insert((i, j));
-            'x_loop: while let Some(cur) = que.pop_front() {
-                let ci = (cur.0).0;
-                let cj = (cur.0).1;
-
-                for d in &dir {
-                    let next_i_i = ci as isize + d.0;
-                    let next_j_i = cj as isize + d.1;
-                    if next_i_i < 0 || next_i_i >= n as isize || next_j_i < 0 || next_j_i >= n as isize {
-                        continue;
-                    }
-
-                    // TODO: 上下左右に直進する方向であればもう結べる
-                    //       愚直移動は手数を圧迫するので移動数は減らしたい
-                    let next_i_u = next_i_i as usize;
-                    let next_j_u = next_j_i as usize;
-                    let next_ij = (next_i_u, next_j_u);
-                    if cable[next_i_u][next_j_u] == cnn[i][j] {
-                        // goal
-                        for ei in 0..cur.1.len() - 1 {
-                            x_move.push(Move(cur.1[ei].0, cur.1[ei].1, cur.1[ei+1].0, cur.1[ei+1].1));
-                        }
-                        let tmp = cnn[i][j];
-                        cnn[i][j] = cnn[ci][cj];
-                        cnn[ci][cj] = tmp;
-                        is_orphaned[ci][cj] = false;
-                        break 'x_loop;
-                    }
-
-                    if visited.contains(&next_ij) || cnn[next_i_u][next_j_u] != '0' {
-                        continue;
-                    }
-
-                    if 2 * (cur.1.len() + x_move.len()) + 1 >= max_k - y_connect.len() {
-                        // 移動制限かかりそうなのでとりあえず諦める
-                        break 'x_loop;
-                    }
-
-                    visited.insert(next_ij);
-                    let mut edges = cur.1.clone();
-                    edges.push(next_ij);
-                    que.push_back((next_ij, edges));
-                }
-            }
-        }
-    }
-    let (mut y_connect, _cable) = greedy_ans(max_k - x_move.len(), &cnn);
-    assert!(x_move.len() + y_connect.len() <= max_k);
-
-    // cable は愚直解用であり不要
+    // 初期状態
+    let (mut y_connect, _cable) = greedy_ans(max_k, &cnn);
     let mut ans_score = calc_score(&cnn, &y_connect);
 
     // 山登り法: 適当に移動させてスコアが上がるようなら上げてやる
@@ -292,11 +217,6 @@ fn main() {
         cur_x_move.push(Move(move_from.0, move_from.1, next_i_u, next_j_u));
         cur_cnn[next_i_u][next_j_u] = cur_cnn[move_from.0][move_from.1];
         cur_cnn[move_from.0][move_from.1] = '0';
-
-        // HACK: "来た道を戻る" 場合にはその移動を消す
-        //       大回りした場合を考慮したい, 例えば右に移動する際に -> 上 -> 右 -> 下 と
-        //       大掛かりに遷移してくる可能性がある
-        //       辺削除が入る場合には残りの移動に制約に反するものが生まれてしまう可能性がある
 
         // スコアの計算と比較
         let (cur_y_connect, _cable) = greedy_ans(max_k - cur_x_move.len(), &cur_cnn);
