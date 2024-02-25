4007adf4ee8c75537456683be3b425dc250d58a3
yammmt <cu.cs.y20@gmail.com>
2024-02-25 11:10:01 +0900

    refactor(AHC030): Remove unnecessary code

--- ahc030/src/bin/a.rs
+++ ahc030/src/bin/a.rs
@@ -1,7 +1,7 @@
 use proconio::{input, source::line::LineSource};
 use rand::rngs::SmallRng;
 use rand::{Rng, SeedableRng};
-use std::collections::{HashSet, VecDeque};
+use std::collections::VecDeque;
 use std::io::{stdout, Write};
 
 fn main() {
@@ -15,7 +15,7 @@ fn main() {
         _eps: f64,
     }
     let mut reserves_sum = 0;
-    let mut unfound_polys = HashSet::new();
+    let mut polys = Vec::with_capacity(m);
     // 向きがわかっているので回転は不要
     for _ in 0..m {
         input! {
@@ -32,7 +32,7 @@ fn main() {
             }
             v.push((i, j));
         }
-        unfound_polys.insert(v);
+        polys.push(v);
     }
 
     let dir = [(-1, 0), (0, -1), (1, 0), (0, 1)];
@@ -87,7 +87,6 @@ fn main() {
     let mut reserves_found_sum = 0;
     let mut stack = VecDeque::new();
     stack.push_back((n / 2, n / 2));
-    let mut poly_cur = vec![];
     let mut could_answer = false;
 
     let mut insert_beginning_point =
@@ -104,7 +103,7 @@ fn main() {
         };
 
     for turn_cur in 0..turn_max {
-        println!("#c turn: {turn_cur}");
+        println!("#c turn: {turn_cur}, found: {reserves_found_sum}/{reserves_sum}");
 
         if could_answer {
             let mut ans = vec![];
@@ -164,7 +163,6 @@ fn main() {
 
             // 探索候補を足す
             if v > 0 {
-                poly_cur.push((p_x, p_y));
                 for d in &dir {
                     let x_nxt = p_x.wrapping_add_signed(d.0);
                     let y_nxt = p_y.wrapping_add_signed(d.1);
@@ -178,67 +176,18 @@ fn main() {
             // 既に探索手詰まりになっている可能性がある, ここで探索可能点が出るまで更新する
             // TODO: ポリオミノの向きがわかっているので, 埋まっている可能性がある探索の方向が絞れる
             // TODO: 埋まっている率が高い点がわかると 0-1BFS で多少の高速化ができる
-            let mut poly_search_completed = false;
             if stack.is_empty() {
-                poly_search_completed = true;
                 insert_beginning_point(&mut stack, &reserves);
             } else {
                 let mut point_dig = stack.pop_back().unwrap();
                 while reserves[point_dig.0][point_dig.1].is_some() {
                     if stack.is_empty() {
-                        poly_search_completed = true;
                         insert_beginning_point(&mut stack, &reserves);
                     }
 
                     point_dig = stack.pop_back().unwrap();
                 }
                 stack.push_back(point_dig);
-            }
-
-            if poly_search_completed {
-                println!("#c   poly_search_completed");
-                // HACK: 一々見つかったポリオミノから全体図を作るのでは判定遅い
-                //       が, 実行時間にかなり余裕があるのでほっとく
-                let mut poly_cur_map = vec![vec![false; n]; n];
-                for &p in &poly_cur {
-                    poly_cur_map[p.0][p.1] = true;
-                }
-                if !poly_cur.is_empty() {
-                    println!("#c   poly_cur_map:");
-                    for pm in &poly_cur_map {
-                        println!("#c    {:?}", pm);
-                    }
-                }
-
-                let mut found_polys = vec![];
-                for vpoly in &unfound_polys {
-                    'i_loop: for i in 0..n {
-                        for j in 0..n {
-                            // 左上 (i, j)
-                            let mut contained = true;
-                            for p in vpoly {
-                                let i_cur = p.0 + i;
-                                let j_cur = p.1 + j;
-                                if i_cur >= n || j_cur >= n || !poly_cur_map[i_cur][j_cur] {
-                                    contained = false;
-                                    break;
-                                }
-                            }
-
-                            if contained {
-                                found_polys.push(vpoly.clone());
-                                break 'i_loop;
-                            }
-                        }
-                    }
-                }
-
-                println!("#c   found_polys.len(): {}", found_polys.len());
-                for fp in found_polys {
-                    unfound_polys.remove(&fp);
-                }
-
-                poly_cur.clear();
             }
         }
     }
