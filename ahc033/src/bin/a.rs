141026ac248c2159f8d6b4c9effe1fa25bf55cd4
yammmt <cu.cs.y20@gmail.com>
2024-05-20 21:28:51 +0900

    feat(AHC033): Keep all boards statuses

--- ahc033/src/bin/a.rs
+++ ahc033/src/bin/a.rs
@@ -70,12 +70,31 @@ fn main() {
     let mut ans = vec![vec![]; CRANE_NUM];
 
     // 盤面管理
-    let mut board = vec![vec![BoardStatus::Empty; 5]; 5];
+    let mut board = vec![vec![vec![BoardStatus::Empty; 5]; 5]; TURN_MAX];
 
     // 進捗管理
     let mut aidx = vec![0; n];
     let mut container_goal_num = 0;
     let mut goal_want = [Some(0), Some(5), Some(10), Some(15), Some(20)];
+
+    // 大クレーンが運ぶ数字を五択
+    // 大クレーン最短経路中に小クレーンも左端を空ける or 次の数字を準備するをする
+    // 大クレーンゴール時に盤面のスコアを出す
+    // 大クレーンをなるべく使わない方がよい気がするな
+    // 一気に全部吐き出すと小クレーンの経路が大きく制限されてしまう
+    // 初期に吐き出すパスを偶数 or 奇数行にすれば, 必ず 0 を引ける？
+    // 一ターンずつ操作を決定するより, 一つのクレーンを決める => 余った経路でうまく残りのクレーンを動かす,
+    // とした方がトータルでは賢いみたい
+
+    // 0-origin 偶数行目に idx-0 があれば, 0 列目登場時点で小さいレーン 1 or 3 をゴールまで導く
+    // 偶数レーンは四列目埋まるまでは何も考えず掃き出す
+
+    // 四列掃き出し後に最もゴールに近いものを運ぶ
+    // 0000 を手計算してもどうにも大クレーン依存が強く前半ほぼほぼ動けない
+    // とはいえども後半はそこそこ改善できる
+    //     => 一旦実装する？
+    // なんいせよ全手の盤面を保存しないとだめっぽい
+    // 最終的な盤面の状態を記憶しておき, 遡って動かす
 
     // とりあえず 3 列目までの 20 個を全部出して, 残りは %5 が小さいものから順に突っ込む
     // 搬出先と順番さえあっていれば, 大クレーンで一つずつ誘導するとして,
@@ -88,9 +107,9 @@ fn main() {
         for c in &init_move {
             ans[i].push(*c);
         }
-        board[i][3] = BoardStatus::Container(ann[i][0]);
-        board[i][2] = BoardStatus::Container(ann[i][1]);
-        board[i][1] = BoardStatus::Container(ann[i][2]);
+        board[init_move.len()][i][3] = BoardStatus::Container(ann[i][0]);
+        board[init_move.len()][i][2] = BoardStatus::Container(ann[i][1]);
+        board[init_move.len()][i][1] = BoardStatus::Container(ann[i][2]);
         aidx[i] = 3;
     }
     // 前処理のため開始位置が固定
@@ -108,17 +127,25 @@ fn main() {
     while turn_cur <= TURN_MAX && container_goal_num < n * n {
         debug!("turn: {turn_cur}");
         turn_cur += 1;
+        for i in 0..n {
+            for j in 0..n {
+                board[turn_cur][i][j] = board[turn_cur - 1][i][j];
+            }
+        }
+        for i in 0..n {
+            debug!("  {:?}", board[turn_cur][i]);
+        }
 
         // 流れてきたものを受け取る
         for i in 0..n {
-            if board[i][0] != BoardStatus::Empty || aidx[i] >= n {
+            if board[turn_cur - 1][i][0] != BoardStatus::Empty || aidx[i] >= n {
                 // コンテナが存在するので受け取れない
                 // "コンテナを掴んだ状態のクレーン" は BoardStatus では区別しないので
                 // これだけ見れば良い
                 continue;
             }
 
-            board[i][0] = BoardStatus::Container(ann[i][aidx[i]]);
+            board[turn_cur][i][0] = BoardStatus::Container(ann[i][aidx[i]]);
             aidx[i] += 1;
         }
 
@@ -143,7 +170,7 @@ fn main() {
                 CraneStrategy::Pick(cid) => {
                     match *c {
                         CraneStatus::BigEmpty(p0, p1) => {
-                            if board[p0][p1] == BoardStatus::Container(cid) {
+                            if board[turn_cur][p0][p1] == BoardStatus::Container(cid) {
                                 debug!("p0: {p0}, p1: {p1}, cid: {cid}");
                                 // 現在位置があっていれば pick
                                 ans[i].push('P');
@@ -159,7 +186,7 @@ fn main() {
                                     let mut tmp_goal_dist = usize::MAX / 2;
                                     for ii in 0..n {
                                         for jj in 0..n-1 {
-                                            if board[ii][jj] == BoardStatus::Empty {
+                                            if board[turn_cur][ii][jj] == BoardStatus::Empty {
                                                 let dist_x = p0.max(ii) - p0.min(ii);
                                                 let dist_y = p1.max(jj) - p1.min(jj);
                                                 if dist_x + dist_y <= tmp_goal_dist {
@@ -173,7 +200,7 @@ fn main() {
                                 };
 
                                 // TODO: 大クレーン以外を考えるとエンバグするかも
-                                board[p0][p1] = BoardStatus::Empty;
+                                board[turn_cur][p0][p1] = BoardStatus::Empty;
                             } else {
                                 // 近い側に動く
                                 // TODO: 目標位置は都度検索すべきでないのでは？
@@ -181,7 +208,7 @@ fn main() {
                                 let mut cid_j = 0;
                                 for ii in 0..n {
                                     for jj in 0..n {
-                                        if board[ii][jj] == BoardStatus::Container(cid) {
+                                        if board[turn_cur][ii][jj] == BoardStatus::Container(cid) {
                                             cid_i = ii;
                                             cid_j = jj;
                                         }
@@ -222,7 +249,7 @@ fn main() {
                                         Some(goal_want[goal_i].unwrap() + 1)
                                     };
                                 } else {
-                                    board[p0][p1] = BoardStatus::Container(cid);
+                                    board[turn_cur][p0][p1] = BoardStatus::Container(cid);
                                 }
                             } else {
                                 debug!("  {p0},{p1} -> {goal_i},{goal_j}");
@@ -254,7 +281,7 @@ fn main() {
                     let mut next_cid = None;
                     for ii in 0..n {
                         for jj in 0..n {
-                            if let BoardStatus::Container(cid) = board[ii][jj] {
+                            if let BoardStatus::Container(cid) = board[turn_cur][ii][jj] {
                                 for want in &goal_want {
                                     if let Some(w) = want {
                                         if cid == *w {
@@ -277,12 +304,13 @@ fn main() {
                                 if let Some(w) = want {
                                     if ann[ii][aidx[ii]] == *w {
                                         // これを置きたい
-                                        assert!(board[ii][0] != BoardStatus::Empty);
+                                        assert!(board[turn_cur][ii][0] != BoardStatus::Empty);
 
                                         // 適当に周囲の空きマスにもっていかせる
                                         // この書き方だと何度か上書きされ得るが,
                                         // 実際の性能にはあまり効かないはず
-                                        if let BoardStatus::Container(cid) = board[ii][0] {
+                                        if let BoardStatus::Container(cid) = board[turn_cur][ii][0]
+                                        {
                                             next_cid = Some(cid);
                                         }
                                     }
