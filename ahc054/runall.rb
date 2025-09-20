055e07961894c7abfe020723521e3f676f3c3835
yammmt <cu.cs.y20@gmail.com>
2025-09-20 21:14:39 +0900

    chore(AHC054): write all outputs to each debug file
    This style is the same as the official visualizer.

--- ahc054/runall.rb
+++ ahc054/runall.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
 INPUT_PTRN = "tools/in/*.txt"
-OUT_FILE = "scores.txt"
-MY_ANS_FILE = "myans.txt"
+OUT_SCORE_FILE = "scores.txt"
+OUT_DIR = "tools/out"
 
 files_in = Dir.glob(INPUT_PTRN).sort
 
-File.open(OUT_FILE, 'w') do |fout|
-    i = 0
+File.open(OUT_SCORE_FILE, 'w') do |fout|
     files_in.each do |fin|
-        File.delete(MY_ANS_FILE) if File.exist?(MY_ANS_FILE)
+        fout_each = File.join(OUT_DIR, File.basename(fin))
+        File.delete(fout_each) if File.exist?(fout_each)
         # "Score = xxx" -> stderr -> stdout
-        # other prints -> stdout -> null
-        s = `../target/release/tester ../target/release/a < #{fin} 2>&1 1>/dev/null`
+        # other prints -> fout_each
+        s = `../target/release/tester ../target/release/a < #{fin} 2>&1 1>#{fout_each}`
 
         # remove 8 characters ("Score = ")
         fout.write(s.slice(8, s.length))
