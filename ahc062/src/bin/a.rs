a7e7e7c1e99d2a4ef09e0e146c8260e63aecf739
yammmt <cu.cs.y20@gmail.com>
2026-04-18 14:17:10 +0900

    feat(AHC062): choose adjacent cells only in random process

--- ahc062/src/bin/a.rs
+++ ahc062/src/bin/a.rs
@@ -1,11 +1,23 @@
 use proconio::fastout;
 use proconio::input;
 use rand::rngs::SmallRng;
+use rand::seq::IndexedRandom;
 use rand::{Rng, SeedableRng};
 use std::time::{Duration, Instant};
 
 // 固定
 const N: usize = 200;
+const DIRS: [(isize, isize); 9] = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 0),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
 
 const TIME_LIMIT_MS: u64 = 2950;
 
@@ -93,10 +105,12 @@ fn main() {
     // 初期経路の最初と最後のマスを固定…ではなんか深みのある探索が必要になりそうで, 時間割けばできるけど嫌
 
     let mut cur_path = [(0, 0); N * N];
+    let mut path_order = [[0; N]; N];
     for i in 0..N {
         for j in 0..N {
             let jj = if i % 2 == 0 { j } else { N - j - 1 };
             cur_path[twod_to_oned(i, j)] = (i, jj);
+            path_order[i][j] = i * N + j;
         }
     }
     let mut cur_score = calc_score(0, N * N - 1, &cur_path, &ann);
@@ -104,24 +118,39 @@ fn main() {
     let mut ans_path = cur_path.clone();
     let mut ans_score = cur_score;
     while start_time.elapsed() < Duration::from_millis(TIME_LIMIT_MS) {
-        // TODO: おそらくほとんど反転不可
-        let i = rng.random_range(0..N * N);
-        let j = rng.random_range(0..N * N);
-        if !could_reversed(i, j, &cur_path) {
+        // 隣接マスを選択
+        // i ではなく i-1 に対する隣接を見る
+        let i = rng.random_range(1..N * N);
+        let d = DIRS.choose(&mut rng).unwrap();
+        let pos_prev = cur_path[i - 1];
+        let pos_j = (
+            pos_prev.0.wrapping_add_signed(d.0),
+            pos_prev.1.wrapping_add_signed(d.1),
+        );
+        if pos_j.0 >= N || pos_j.1 >= N {
             continue;
         }
 
-        // とりあえず山登り
+        let j = path_order[pos_j.0][pos_j.1];
 
-        let score_diff = calc_reverse_score_diff(i, j, &cur_path, &ann);
+        // i と j の大小関係を整理
+        let (start, end) = if i < j { (i, j) } else { (j, i) };
+
+        if !could_reversed(start, end, &cur_path) {
+            continue;
+        }
+
+        let score_diff = calc_reverse_score_diff(start, end, &cur_path, &ann);
 
         if score_diff > 0 {
-            // TODO: 遅い
-            cur_path[i..=j].reverse();
+            cur_path[start..=end].reverse();
+            for k in start..=end {
+                let (r, c) = cur_path[k];
+                path_order[r][c] = k;
+            }
             cur_score = (cur_score as isize + score_diff) as usize;
 
             if cur_score > ans_score {
-                // TODO: 遅い
                 ans_path = cur_path.clone();
                 ans_score = cur_score;
             }
